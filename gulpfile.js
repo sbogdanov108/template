d1e7754471c0509369f886113bb4cff2e698db73
Sergey Bogdanov <bgrodthai@gmail.com>
2015-07-08 20:07:30 +0300

    Added structure of the template

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Sergey on 04.07.2015.
+ */
+
+'use strict';
+
+var gulp = require('gulp'),
+    notify = require('gulp-notify'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    mainBowerFiles = require('main-bower-files');
+
+// css
+gulp.task('css', function ()
+{
+    return gulp.src('scss/*.scss')
+        .pipe(sass(
+            {
+                outputStyle: 'expanded'
+            })
+            .on('error', notify.onError(
+            {
+                message: "<%= error.message %>",
+                title: "Error!"
+            })
+        ))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('app/css'))
+        .pipe(notify('Good work!'));
+});
+
+// watch
+gulp.task('watch', ['css'], function ()
+{
+    gulp.watch('scss/*.scss', ['css']);
+});
+
+// default
+gulp.task('default', ['watch']);
+
+// get main js files from bower
+gulp.task('mainJS', function ()
+{
+    return gulp.src(mainBowerFiles('**/*.js'))
+        .pipe(gulp.dest('app/js'))
+});
+
+// get main css files from bower
+gulp.task('mainCSS', function ()
+{
+    return gulp.src(mainBowerFiles('**/*.css'))
+        .pipe(gulp.dest('app/css'))
+});
